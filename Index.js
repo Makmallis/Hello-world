7f6ad2503782f06f154ac71160038b48d47d6e07
Makmallis <makhs.mntungwa1@icloud.com>
2025-11-08 22:53:27 +0000

    changes

--- Index.js
+++ Index.js
@@ -1,1 +1,17 @@
-console.log(`Hello`);
+//console.log(`Hello`);
+//console.log(`I like pizza!`)
+
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza`);
+
+
+document.getElementById("myH1").textcontent = `Hello`;
+document.getElementById("myP").textcontent = `I like pizza`;
+//This is a comment 
+
+/*
+   This
+   is
+   a
+   comment
+*/
