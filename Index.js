b0ab661ac15ee0d5bbd9bd68c22bcb0cf3eb411c
Makmallis <makhs.mntungwa1@icloud.com>
2025-11-09 00:56:16 +0000

    changes

--- Index.js
+++ Index.js
@@ -19,16 +19,21 @@ document.getElementById("myP").textcontent = `I like pizza`;
 //1.Declaration let x;
 //2.Assignment  x= 100;
 
-let x;
-x = 100;
-let age = 19;
-let gpa = 4.3;
+//let x;
+//x = 100;
+//let age = 19;
+//let gpa = 4.3;
 
-console.log(`Congradulations Abigail you have maintained a ${gpa}GPA, we are so proud`);
-console.log(`You are ${age} today, Happy birthday`);
- //strings 
+//console.log(`Congradulations Abigail you have maintained a ${gpa}GPA, we are so proud`);
+//console.log(`You are ${age} today, Happy birthday`);
+ 
+//strings 
+
  let firstName = "Mandy";
-
+ let favouriteFood = "Pepperoni pizza";
+ let email = "Mandy123@gmail.com";
  console.log(typeof firstName);
  console.log(`Your name is ${firstName}`);
- 
+ console.log(`My brother makes the best ${favouriteFood}, he uses fresh goat cheese`);
+ console.log(`Please do reach out to me further on my email ${email}`);
+
